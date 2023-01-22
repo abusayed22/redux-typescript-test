a3420c7aa6ea2fbd52d00853ebed9a9abfe0c7f0
abusayed22 <abusayed.in22@gmail.com>
2023-01-22 19:24:12 +0600

    desgin complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className='w-[450px]  h-[400px] outline-none border border-orange-500 mx-auto my-[100px]'>
+          <input type="text" className='w-[380px] outline-none hover:outline-blue-400 h-32 bg-yellow-200 mx-8 my-5 p-5 text-blue-900 text-4xl font-bold text-center'/>
+          <button className='w-[380px] h-10 bg-green-200 mx-8 my-5'>Deposit</button>
+          <button className='w-[380px] h-10 bg-red-200 mx-8'>Withdrow</button>
+          <button className='w-[380px] h-10 bg-orange-200 mx-8 my-5'>Bankrupt</button>
+        </div>
     </div>
   );
 }
